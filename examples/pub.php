3f14d464eee3abb16eb372567df6583ffd95265b
Michael Maclean <michael@no-surprises.co.uk>
2013-10-04 15:02:44 +0100

    Add a couple of examples

---
+++ examples/pub.php
@@ -0,0 +1,36 @@
+<?php
+
+$client = new Mosquitto\Client();
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->connect("localhost", 1883, 5);
+$client->subscribe('/#', 1);
+
+while (true) {
+	$client->loop();
+	$client->publish('/hello', "Hello from PHP at " . date('Y-m-d H:i:s'), 1, 0);
+	$client->loop();
+
+	sleep(2);
+}
+
+$client->disconnect();
+unset($client);
+
+function connect($r) {
+	echo "I got code {$r}\n";
+}
+
+function subscribe() {
+	echo "Subscribed to a topic\n";
+}
+
+function message($message) {
+	printf("Got a message on topic %s with payload:\n%s\n\n", $message->topic, $message->payload);
+}
+
+function disconnect() {
+	echo "Disconnected cleanly\n";
+}
---
+++ examples/testwill.php
@@ -0,0 +1,28 @@
+<?php
+
+$client = new Mosquitto\Client();
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->setWill('/hello', "Client died :-(", 1, 0);
+$client->connect("localhost", 1883, 5);
+$client->subscribe('/#', 1);
+
+$client->loopForever();
+
+function connect($r) {
+	echo "I got code {$r}\n";
+}
+
+function subscribe() {
+	echo "Subscribed to a topic\n";
+}
+
+function message($message) {
+	printf("Got a message on topic %s with payload:\n%s\n", $message->topic, $message->payload);
+}
+
+function disconnect() {
+	echo "Disconnected cleanly\n";
+}

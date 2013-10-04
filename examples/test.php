6257e32f1112f8b409d1c35dade76ffae8739e6c
Michael Maclean <michael@no-surprises.co.uk>
2013-10-04 14:22:41 +0100

    Test disconnect callback

--- examples/test.php
+++ examples/test.php
@@ -1,15 +1,18 @@
 <?php
 
-$client = new Mosquitto\Client("test");
+$client = new Mosquitto\Client();
 $client->onConnect('callback');
+$client->onDisconnect('disconnect');
 $client->onSubscribe('subscribe');
 $client->onMessage('message');
 $client->connect("localhost", 1883, 5);
 $client->subscribe('/#', 1);
 
-while (true) {
+for ($i = 0; $i < 10; $i++) {
 	$client->loop();
 }
+$client->disconnect();
+unset($client);
 
 function callback($r) {
 	echo "I got code {$r}\n";
@@ -21,4 +24,8 @@ function subscribe() {
 
 function message($message) {
 	var_dump($message->topic, $message->payload);
+}
+
+function disconnect() {
+	echo "Disconnected cleanly\n";
 }

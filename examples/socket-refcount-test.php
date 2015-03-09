5e88120c993e298b221fc8270e8cda22b1169db1
Michael Maclean <michael.maclean@stv.tv>
2015-03-09 14:07:50 +0000

    Move loop() to after the select

--- examples/socket-refcount-test.php
+++ examples/socket-refcount-test.php
@@ -8,9 +8,9 @@ $client->onConnect(function() use ($client) {
 $client->connect('localhost');
 
 while (true) {
-    $client->loop();
     $socket = $client->getSocket();
     var_dump($socket);
     $r = $w = $e = [$socket];
     $changedSockets = socket_select($r, $w, $e, 30);
+    $client->loop();
 }
